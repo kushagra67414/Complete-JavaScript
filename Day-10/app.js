5a6407162bc629185b56f0dadefdde3c15ecf546
kushagra67414 <500067414@stu.upes.ac.in>
2020-07-09 23:36:03 +0530

    let vs var  WAR

---
+++ Day-10/app.js
@@ -0,0 +1,27 @@
+var name = 'Max';
+
+if (name === 'Max') {
+  var hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name , hobbies);
+
+greet();
+
+//strict mode
+
+'use strict';
+ username = 'kushagra';
+var undefine = '5';
+ console.log(username);
+
+
+//let is usd for specific block   
